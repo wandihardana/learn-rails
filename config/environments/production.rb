62a954af3e90de342056d6f8d18daeca0b6d9344
Wandi Hardana <wandi.hardana@gmail.com>
2016-11-16 04:17:11 +0000

    fix typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,12 @@ LearnRails::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["DOMAIN_NAME"],
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
 end
